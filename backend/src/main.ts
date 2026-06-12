9b5cb64cc49198ed275242b0a0d2a40a229df4a0
Jhonatan Muchavisoy <jhonatan.muchavisoy1809@gmail.com>
2026-06-12 17:58:01 -0500

    fix: CORS hardcodeado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      process.env.FRONTEND_PRODUCTION_URL || '',
+      'https://electiva3-frontend.onrender.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
